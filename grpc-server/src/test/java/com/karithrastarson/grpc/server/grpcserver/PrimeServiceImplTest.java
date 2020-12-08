8148b383f8296967989d8ee7083c2dbd7fbd401d
karithrastarson <karithrastarson@gmail.com>
2020-12-08 14:31:09 +0000

    Add a very basic unit test to validate the prime logic.

---
+++ grpc-server/src/test/java/com/karithrastarson/grpc/server/grpcserver/PrimeServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.karithrastarson.grpc.server.grpcserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+public class PrimeServiceImplTest {
+
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        public PrimeServiceImpl primeServiceImpl() {
+            return new PrimeServiceImpl();
+        }
+    }
+
+    @Autowired
+    PrimeServiceImpl primeService;
+
+    @Test
+    public void testPrime() {
+        String primes = primeService.primes(1);
+        assertEquals("", primes);
+
+        primes = primeService.primes(10);
+        assertEquals("2, 3, 5, 7", primes);
+    }
+}
